d269a99e31a1c969c08d5cafefdd26066a52a205
Reinman <matt.rein@mlb.com>
2017-05-22 16:17:41 -0600

    Update PhotoRetreiver to be auto-injectable, and inject the instance into MainActivity.

--- app/src/main/java/com/mattrein/photoapp/api/PhotoRetriever.kt
+++ app/src/main/java/com/mattrein/photoapp/api/PhotoRetriever.kt
@@ -1,26 +1,18 @@
 package com.mattrein.photoapp.api
 
+import com.mattrein.photoapp.dagger.ActivityScope
 import com.mattrein.photoapp.models.PhotoList
 import retrofit2.Callback
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
 /**
  * Retriever for getting data via the PhotoAPI interface.
  */
-class PhotoRetriever {
+@ActivityScope
+class PhotoRetriever
+@Inject constructor(private val service: PhotoApi) {
 
-    private val service: PhotoApi
-
-    init {
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://pixabay.com/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        service = retrofit.create(PhotoApi::class.java)
-    }
-
-    fun getPhotos(callback : Callback<PhotoList>) {
+    fun getPhotos(callback: Callback<PhotoList>) {
         val call = service.getPhotos()
         call.enqueue(callback)
     }
